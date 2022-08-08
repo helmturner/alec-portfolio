2978a5737c33b5f7ad5d17ab8622eab6420e9716
Alec Helmturner <git@alecvision.com>
2022-08-08 22:52:57 +0000

    add first pass mantine components

---
+++ components/withMantine/AppLayout.tsx
@@ -0,0 +1,188 @@
+import {
+  AppShellProps,
+  Aside,
+  Box,
+  Burger,
+  Footer,
+  Group,
+  Header,
+  Navbar,
+  NavLink,
+} from "@mantine/core";
+import { useClickOutside } from "@mantine/hooks";
+import { useState } from "react";
+import Image from "next/future/image";
+import glasses from "../public/images/glasses.svg";
+import {
+  bioParagraphs,
+  firstName,
+  gitHubUrl,
+  lastName,
+  linkedInUrl,
+} from "../../constants";
+import Link from "next/link";
+import linkedin from "../public/images/LinkedIn-Logos/LI-In-Bug.png";
+import octocat from "../public/images/GitHub-Mark/GitHub-Mark-32px.png";
+
+const AppLayout = ({ children }: AppShellProps) => {
+  const [navOpen, setNavOpen] = useState(false);
+  const navRef = useClickOutside(() => setNavOpen(false));
+  return (
+    <Box
+      sx={{
+        display: "grid",
+        gridTemplateColumns: "repeat(20, minmax(1ch, 1fr))",
+        gridTemplateRows: "repeat( auto-fit, minmax(20px, 1fr) )",
+        "> *": {
+          gridColumn: "1 / -1",
+          gridRow: "3 / -2",
+          display: "grid",
+          gridTemplateColumns: "subgrid",
+          gridTemplateRows: "subgrid",
+          "> * ": {
+            gridColumn: "1 / -1",
+          },
+        },
+      }}
+    >
+      <Group
+        sx={{
+          gridColumn: "4 / span 14",
+          gridRow: "1 / span 1",
+          display: "block",
+          position: "relative",
+          textAlign: "center",
+          top: 0,
+          small: {
+            position: "static",
+            fontWeight: "bold",
+            fontSize: "1.2rem",
+            margin: ".1em",
+          },
+          img: {
+            width: "auto",
+            height: "clamp(60px, 35vw, 30vh)",
+            justifySelf: "center",
+          },
+        }}
+      >
+        <small>{"You're watching"}</small>
+        <br />
+        <Image
+          src={glasses}
+          priority={true}
+          placeholder="empty"
+          alt="AlecVision"
+        />
+      </Group>
+      <Header
+        unstyled
+        height="auto"
+        sx={{
+          display: "block",
+          gridColumn: "4 / span 14",
+          gridRow: "2 / span 1",
+          justifySelf: "center",
+          h1: {
+            marginBlock: ".1em",
+            position: "sticky",
+            top: "0",
+          },
+        }}
+      >
+        <h1>
+          <strong>AlecVision</strong>
+        </h1>
+        <Burger
+          sx={{ display: "none" }}
+          opened={navOpen}
+          onClick={() => setNavOpen(!navOpen)}
+        />
+      </Header>
+      <Navbar
+        data-nav-open={navOpen}
+        sx={{
+          display: "flex",
+          position: "sticky",
+          height: "100%",
+          bottom: 0,
+          alignSelf: "self-start",
+          gridColumn: "1 / 4",
+          gridRow: "2 / span 2",
+          boxShadow: "2px 0px 10px 2px grey",
+        }}
+        ref={navRef}
+      >
+        <div>
+          <Link href="/" passHref>
+            <NavLink component="a" label="Home" />
+          </Link>
+          <Link href="/projects" passHref>
+            <NavLink component="a" label="Projects" />
+          </Link>
+          <Link href="/resume" passHref>
+            <NavLink component="a" label="Resume" />
+          </Link>
+          <NavLink
+            component="a"
+            href={gitHubUrl}
+            icon={<Image alt="GitHub" src={octocat} height={32} width={32} />}
+          />
+          <NavLink
+            component="a"
+            href={linkedInUrl}
+            icon={
+              <Image alt="LinkedIn" src={linkedin} height={32} width={32} />
+            }
+          />
+        </div>
+      </Navbar>
+      <Aside
+        sx={{
+          display: "flex",
+          position: "sticky",
+          height: "100%",
+          bottom: 0,
+          alignSelf: "self-start",
+          gridColumn: "18 / span 3",
+          gridRow: "2 / span 2",
+          boxShadow: "2px 0px 10px 2px grey, 0 0 0 0",
+        }}
+      >
+        <Group>
+          <h2>Bio</h2>
+          {bioParagraphs.map((paragraph) => (
+            <p key={paragraph}>{paragraph}</p>
+          ))}
+        </Group>
+      </Aside>
+      <Box
+        sx={{
+          display: "flex",
+          gridColumn: "4 / span 14",
+          gridRow: "2 / span 1",
+          "img": {
+            width: "100%",
+            height: "auto"
+          }
+        }}
+      >
+        <main>{children}</main>
+      </Box>
+      <Footer
+        sx={{
+          textAlign: "center",
+          gridColumn: "1 / span 20",
+          gridRow: "3 / 4",
+        }}
+        height="auto"
+      >
+        <small>
+          © {`${new Date().getFullYear()} ${firstName} ${lastName}`}{" "}
+        </small>
+      </Footer>
+    </Box>
+  );
+};
+
+export default AppLayout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,6 @@ import {
   title,
   skills,
 } from "../constants";
-import styles from "../styles/pages/index.module.css";
 import Link from "next/link";
 import Image from "next/future/image";
 import Contact from "../components/Contact";
@@ -34,36 +33,26 @@ const Home: NextPage<PageProps> = ({ projects }) => {
         skills={skills}
         telephone={phone}
       />
-      
-      <aside>
-        <h2>Bio</h2>
-        {bioParagraphs.map((paragraph) => (
-          <p key={paragraph}>{paragraph}</p>
-        ))}
-      </aside>
-
-      <main>
-        <h2>Featured Projects</h2>
-        {projects.map((project, index) => (
-          <section key={index}>
-            <Image src={comingSoonImage} alt={project.title} />
-            <Link href={`projects/${project.slug}`}>
-              <h3>{project.title}</h3>
-            </Link>
-            <h4>
-              <small>{project.subtitle}</small>
-            </h4>
-            <small>
-              Built with:{" "}
-              <ul>
-                {project.tech.map((item, index) => (
-                  <li key={index}>{item.toUpperCase()}</li>
-                ))}
-              </ul>
-            </small>
-          </section>
-        ))}
-      </main>
+      <h2>Featured Projects</h2>
+      {projects.map((project, index) => (
+        <section key={index}>
+          <Image src={comingSoonImage} alt={project.title} />
+          <Link href={`projects/${project.slug}`}>
+            <h3>{project.title}</h3>
+          </Link>
+          <h4>
+            <small>{project.subtitle}</small>
+          </h4>
+          <small>
+            Built with:{" "}
+            <ul>
+              {project.tech.map((item, index) => (
+                <li key={index}>{item.toUpperCase()}</li>
+              ))}
+            </ul>
+          </small>
+        </section>
+      ))}
     </>
   );
 };
